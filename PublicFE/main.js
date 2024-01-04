92a81da0749f6550ff83d9afea7305f0dcad1570
Daniel Griffin <dgriffinpv@gmail.com>
2024-01-03 19:59:30 -0700

    project completed meeting minimum requirements

--- PublicFE/main.js
+++ PublicFE/main.js
@@ -1,2 +1,89 @@
-console.log('JS entered the chat!')
 
+let noteForm = document.getElementById('note-form');
+
+let noteNameInput = document.getElementById('noteName-input')
+
+let forWhoInput = document.getElementById('forWho-input');
+
+let emailInput = document.getElementById('email-input');
+
+let fromInput = document.getElementById('from-input');
+
+let messageinput = document.getElementById('message-input');
+
+let deleteform = document.getElementById('delete-form')
+
+let deleteById = document.getElementById('delete-by-id-input')
+
+noteForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    let body = {
+        name: noteNameInput.value,
+        to: forWhoInput.value,
+        email: emailInput.value,
+        from: fromInput.value,
+        message: messageinput.value
+    };
+
+    
+    axios.post('/create-note', body)
+         .then((response) => {
+            loadNotesToDom(response.data);
+         })
+         .catch((error) => {
+            console.log(error);
+         });
+});
+
+
+axios.get('/notes')
+.then((result) => {
+    loadNotesToDom(result);
+})
+
+.catch((error) => {
+    console.log(error);
+});
+
+deleteform.addEventListener('submit', (event) => {
+    event.preventDefault();
+    let id = deleteById.value
+
+    axios.delete('/delete-note?id=' + id)
+    .then((response) => {
+        loadNotesToDom(response.data)
+        alert('Note removed')
+    })
+    .catch((error) => {
+        console.log(error)
+    })
+});
+
+
+function loadNotesToDom(notesArray){
+    document.getElementById('notes-display').innerHTML = '';
+
+    for (let i = 0; i < notesArray.length; i++){
+        let containerDiv = document.createElement('div');
+        let heading = document.createElement('h3');
+        let toP = document.createElement('p');
+        let emailP = document.createElement('p');
+        let fromP = document.createElement('p');
+        let messageP = document.createElement('p');
+
+        containerDiv.appendChild(heading);
+        containerDiv.appendChild(toP);
+        containerDiv.appendChild(emailP);
+        containerDiv.appendChild(fromP);
+        containerDiv.appendChild(messageP);
+
+        heading.innerHTML = 'Note ID: ' + notesArray[i].name;
+        toP.innerHTML = 'To: ' + notesArray[i].to;
+        emailP.innerHTML = 'Email: ' + notesArray[i].email;
+        fromP.innerHTML = 'From: ' + notesArray[i].from;
+        messageP.innerHTML = 'Message: ' + notesArray[i].message;
+        
+       document.getElementById('notes-display').appendChild(containerDiv);
+    };
+};
--- server.js
+++ server.js
@@ -1,23 +1,47 @@
-import express from "express"
-import { dirname, join } from "path"
-import { fileURLToPath } from "url"
+import express from "express";
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
 
 
-let app = express()
+let app = express();
+
+app.use(express.json());
+
+let db = [];
 
 app.get('/', (req, res) => {
-    res.sendFile(join(dirname(fileURLToPath(import.meta.url)), '/PublicFE/index.html'))
-})
+    res.sendFile(join(dirname(fileURLToPath(import.meta.url)), '/PublicFE/index.html'));
+});
 
 app.get('/js', (req, res) => {
-    res.sendFile(join(dirname(fileURLToPath(import.meta.url)), '/PublicFE/main.js'))
-})
+    res.sendFile(join(dirname(fileURLToPath(import.meta.url)), '/PublicFE/main.js'));
+});
 
 app.get('/css', (req, res) => {
-    res.sendFile(join(dirname(fileURLToPath(import.meta.url)), '/PublicFE/styles.css'))
+    res.sendFile(join(dirname(fileURLToPath(import.meta.url)), '/PublicFE/styles.css'));
+});
+
+app.get('/notes', (req, res) => {
+    res.status(200).send(db);
+})
+
+app.post('/create-note', (req, res) => {
+    db.push(req.body);
+    res.status(200).send(db);
+});
+
+app.delete('/delete-note', (req, res) => {
+    let id = req.query.id
+    for (let i = 0; i < db.length; i++) {
+        if (db[i].id === id){
+            db.splice(i, 1)
+            i--
+        }
+    }
+    res.status(200).send(db)
 })
 
 
 app.listen(8081, () => {
-    console.log('Server cruising on port 8081')
-})
+    console.log('Server cruising on port 8081');
+});
